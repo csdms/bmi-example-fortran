473173b7e248d8bcb77739619c42de3985983366
Mark Piper <mark.piper@colorado.edu>
2024-01-31 21:28:54 -0700

    Add concrete type for bmi_geo

---
+++ bmi_heat/bmi_heat_geo.f90
@@ -0,0 +1,22 @@
+! Implement the bmi_geo type and use the bmi_heat type in a new type, bmi_heat_geo
+
+module bmiheatgeof
+
+    use bmigeof
+    use bmiheatf
+    implicit none
+
+    type, extends (bmi_geo) :: bmi_heat_geo
+        private
+        type (bmi_heat) :: bmi_base
+    contains
+        procedure :: geo_initialize => heat_geo_initialize
+        procedure :: get_grid_coordinate_names => heat_grid_coordinate_names
+        procedure :: get_grid_coordinate_units => heat_grid_coordinate_units
+        procedure :: get_grid_coordinate => heat_grid_coordinate
+        procedure :: get_grid_crs => heat_grid_crs
+    end type bmi_heat_geo
+
+contains
+    
+end module bmiheatgeof
