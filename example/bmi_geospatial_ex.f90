914a405b6f7453d27f4cc8aec47ad44f4270a9ce
Mark Piper <mark.piper@colorado.edu>
2024-02-02 11:51:18 -0700

    Start writing example main program

--- example/bmi_geospatial_ex.f90
+++ example/bmi_geospatial_ex.f90
@@ -1,6 +1,16 @@
 ! An example of using a BMI extension
 program bmi_geospatial_ex
 
+    use bmif_2_0
+    use bmiheatf
+    use bmiheatgeof
     implicit none
-    
+
+    type(bmi_heat) :: h
+    integer :: status
+    character (len=BMI_MAX_COMPONENT_NAME), pointer :: name
+
+    status = h%get_component_name(name)
+    write (*,"(a, a30)") "Component name: ", name
+
 end program bmi_geospatial_ex
